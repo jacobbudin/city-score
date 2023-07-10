bcca0ad937a46e3f312700796ce43fc11f8fa5ba
Gregory LaSpina <grelas@gmail.com>
2023-07-10 13:50:35 -0400

    cleanup

--- src/city_score/sources/traillink.py
+++ src/city_score/sources/traillink.py
@@ -5,36 +5,38 @@ from html.parser import HTMLParser
 import requests
 
 class TrailLinkMilesParser(HTMLParser):
-    def __init__(self, class_names):
+    def __init__(self):
         super().__init__()
-        self.class_names = class_names
-        self.capture_data = False
+        self.found = False
         self.total_trail_miles = 0
 
     def handle_starttag(self, tag, attrs):
         if tag == "span":
             for attr in attrs:
                 if attr[0] == "itemprop" and attr[1] == "distance":
-                    self.capture_data = True
+                    self.found = True
 
     def handle_data(self, data):
-        if self.capture_data:
+        if self.found:
             self.total_trail_miles += float(data.split()[0])
 
     def handle_endtag(self, tag):
-        if tag == "span" and self.capture_data:
-            self.capture_data = False
+        if tag == "span" and self.found:
+            self.found = False
 
 
 class TrailLink(Source):
     base_url = "https://www.traillink.com/trailsearch"
 
-@dimension('TrailLink')
+def format(number):
+    return f"{number} miles"
+
+@dimension('TrailLink Mileage')
 def trail_total_miles(city):
     key = 'traillink-%s' % (str(city))
     count = cache.get(key)
     if count is not None:
-        return count
+        return format(count)
     
     response = requests.get(TrailLink.base_url, params={
         "city": city.name,
@@ -46,11 +48,11 @@ def trail_total_miles(city):
         parser.feed(response.text)
         count = int(parser.total_trail_miles)
         cache.set(key, count)
-        return count
+        return format(count)
 
 @scorer('TrailLink')
 def trail_total_miles_scorer(city, lower, upper):
-    count = trail_total_miles(city)
+    count = float(trail_total_miles(city).split(' ')[0])
     if count >= upper:
         return 100
     if count >= lower:
