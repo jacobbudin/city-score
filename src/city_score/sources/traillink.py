da9fbcd64a5e25e0114ae7b624df68fbf690f666
Gregory LaSpina <grelas@gmail.com>
2023-07-10 13:32:08 -0400

    use sum miles

--- src/city_score/sources/traillink.py
+++ src/city_score/sources/traillink.py
@@ -4,18 +4,33 @@ from ..source import Source
 from html.parser import HTMLParser
 import requests
 
-class InputValueAttributeParser(HTMLParser):
+class TrailLinkMilesParser(HTMLParser):
+    def __init__(self, class_names):
+        super().__init__()
+        self.class_names = class_names
+        self.capture_data = False
+        self.total_trail_miles = 0
+
     def handle_starttag(self, tag, attrs):
-        if tag == "input":
-            attrs_dict = dict(attrs)
-            if attrs_dict.get("name") == "ResultsCount":
-                self.results_count_value = attrs_dict.get("value")
+        if tag == "span":
+            for attr in attrs:
+                if attr[0] == "itemprop" and attr[1] == "distance":
+                    self.capture_data = True
+
+    def handle_data(self, data):
+        if self.capture_data:
+            self.total_trail_miles += float(data.split()[0])
+
+    def handle_endtag(self, tag):
+        if tag == "span" and self.capture_data:
+            self.capture_data = False
+
 
 class TrailLink(Source):
     base_url = "https://www.traillink.com/trailsearch"
 
 @dimension('TrailLink')
-def trail_count(city):
+def trail_total_miles(city):
     key = 'traillink-%s' % (str(city))
     count = cache.get(key)
     if count is not None:
@@ -27,15 +42,15 @@ def trail_count(city):
     })
 
     if response.status_code == 200:
-        parser = InputValueAttributeParser()
+        parser = TrailLinkMilesParser()
         parser.feed(response.text)
-        count = int(parser.results_count_value)
+        count = int(parser.total_trail_miles)
         cache.set(key, count)
         return count
 
 @scorer('TrailLink')
-def trail_count_scorer(city, lower, upper):
-    count = trail_count(city)
+def trail_total_miles_scorer(city, lower, upper):
+    count = trail_total_miles(city)
     if count >= upper:
         return 100
     if count >= lower:
